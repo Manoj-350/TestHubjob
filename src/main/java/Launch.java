37cb76ce09cdea9b76d36c0ddf2b3d7e613b17f0
Manoj Kumar <mady.palawat@gmail.com>
2021-12-04 20:32:41 +0530

    hub 10

--- src/main/java/Launch.java
+++ src/main/java/Launch.java
@@ -96,7 +96,8 @@ WebElement tar2= driver.findElement(By.xpath("//a[contains(text(),'Flash Movie D
 
 //tar2.click();
 
-System.out.println("working fine b9");
+System.out.println("working fine b10");
+System.out.println("working fine b");
 
 
 
